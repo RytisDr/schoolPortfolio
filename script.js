c9a3b1b845af5654d7d4dd83150f9c1184f66321
RytisDr <r.drazdauskas@gmail.com>
2017-12-30 23:38:42 +0100

    Nothing works, going to sleep.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let iAm = document.getElementById("IAm");
 let aboutMe = document.getElementById("about");
 let footer = document.querySelector("footer");
-let backArrow = document.getElementsByClassName("backArrow");
+let backArrow = document.querySelector(".backArrow");
 let themes = document.getElementById("themes");
+let iDid = document.getElementById("IDid");
 
 iAm.addEventListener('click', slide);
 function slide(){
     aboutMe.classList.add("transition");
     footer.style.color = "black";
+    backArrow = "here";
 }
 backArrow.addEventListener('click', back)
 function back(){
+    if(backArrow == "here"){
     aboutMe.classList.remove("transition");
     aboutMe.classList.add("goBack");
     footer.style.color = "white";
     aboutMe.addEventListener("animationend", function(){
         aboutMe.classList.remove("goBack");
     })
+    }else{
+    themes.classList.remove("transition");
+    themes.classList.add("goBack");
+    themes.addEventListener("animationend", function(){
+        themes.classList.remove("goBack");
+    })
     }
-
+}
+iDid.addEventListener('click', slide2);
+function slide2(){
+    themes.classList.add("transition");
+}
