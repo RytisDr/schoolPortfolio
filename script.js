5cca88882a61a2441196892289a11c7f2e435e9b
RytisDr <r.drazdauskas@gmail.com>
2018-01-02 01:10:04 +0100

    Works on desktop, nothing works on mobile.

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let aboutMe = document.getElementById("about");
 let footer = document.querySelector("footer");
 let backArrow = document.querySelectorAll(".backArrow");
 let themes = document.getElementById("themes");
+let theme1Page = document.getElementById("theme1");
 let iDid = document.getElementById("IDid");
+let theme1 = document.getElementById("one");
+let theme2 = document.getElementById("two");
+let theme3 = document.getElementById("three");
+let theme4 = document.getElementById("four");
+let flatDesign = document.getElementById("flatDesign");
 
 iAm.addEventListener('click', slide);
 function slide(){
@@ -27,3 +33,17 @@ iDid.addEventListener('click', slide2);
 function slide2(){
     themes.classList.add("transition");
 }
+theme1.addEventListener('click', open1);
+function open1(){
+    let back = document.getElementById("backFromTheme1");
+    flatDesign.addEventListener('click', function(url){
+       window.open("http://rtsdr.com/kea/web/responsive/");
+    })
+    theme1Page.classList.add("transition");
+    back.addEventListener('click', function(){
+        theme1Page.classList.replace("transition", "goBack");
+        theme1Page.addEventListener('animationend', function(){
+            theme1Page.classList.remove("goBack");
+        })
+    })
+    }
